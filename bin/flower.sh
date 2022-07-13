fc184696be03c350744db9381c27d269446737d3
Grega Vrbančič <grega.vrbancic@gmail.com>
2022-07-13 22:10:11 +0200

    Update flower to 1.0.0

--- bin/flower.sh
+++ bin/flower.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -e
 
 # Defaults
-export LOG_LEVEL="${LOG_LEVEL:-info}"
 export HOST_IP="${HOST_IP:-$(/sbin/ip route|awk '/default/ { print $3 }')}"
 export AMQP_HOST="${AMQP_HOST:-$HOST_IP}"
 export AMQP_ADMIN_HOST="${AMQP_ADMIN_HOST:-$HOST_IP}"
@@ -26,4 +25,4 @@ if [ "$DISCOVER_RABBITMQ" == "true" ]; then
   done
 fi
 
-/usr/local/bin/celery --loglevel="$LOG_LEVEL" flower
+/usr/local/bin/celery flower
