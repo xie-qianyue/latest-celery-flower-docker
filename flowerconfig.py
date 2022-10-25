08e0d0cf9e64c4975ff2a804df3b4bca18e54ce1
xie qianyue <hzxieqianyue@corp.netease.com>
2022-10-25 17:05:50 +0800

    update

--- flowerconfig.py
+++ flowerconfig.py
@@ -1,18 +1,10 @@
 import os
 
-AMPQ_ADMIN_USERNAME = os.getenv('AMQP_ADMIN_USERNAME', 'guest')
-AMPQ_ADMIN_PASSWORD = os.getenv('AMQP_ADMIN_PASSWORD', 'guest')
-AMQP_ADMIN_HOST = os.getenv('AMQP_ADMIN_HOST', '172.17.42.1')
-AMQP_ADMIN_PORT = int(os.getenv('AMQP_ADMIN_PORT', '15672'))
-
-DEFAULT_BROKER_API = 'http://%s:%s@%s:%d/api/' \
-                     % (AMPQ_ADMIN_USERNAME, AMPQ_ADMIN_PASSWORD,
-                        AMQP_ADMIN_HOST, AMQP_ADMIN_PORT)
-USERNAME = os.getenv('USERNAME', 'root')
-PASSWORD = os.getenv('PASSWORD', 'changeit')
+USERNAME = os.getenv('USERNAME', 'admin')
+PASSWORD = os.getenv('PASSWORD', 'qwer;1234')
 
 port = int(os.getenv('FLOWER_PORT', '5555'))
-broker_api = os.getenv('FLOWER_BROKER_API', DEFAULT_BROKER_API)
-max_tasks = int(os.getenv('FLOWER_MAX_TASKS', '3600'))
-basic_auth = [os.getenv('FLOWER_BASIC_AUTH', '%s:%s'
-                        % (USERNAME, PASSWORD))]
+max_tasks = int(os.getenv('FLOWER_MAX_TASKS', '300000'))
+basic_auth = [os.getenv('FLOWER_BASIC_AUTH', '%s:%s'% (USERNAME, PASSWORD))]
+persistent=True
+purge_offline_workers=3600
